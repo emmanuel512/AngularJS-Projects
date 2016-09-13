c29abb13c6cdec2fd7e851ea4d4b3c1b653aa11a
emmanuel512 <emmanud@udel.edu>
2016-09-12 23:22:28 -0400

    Update forecastControl.js

--- Chatham Angular/js/forecastControl.js
+++ Chatham Angular/js/forecastControl.js
@@ -6,30 +6,20 @@ app.controller("forecastControl",function($scope, $http){
 			cityfetch();
 		}
 		else{
-			$scope.cCity = "";
-			$scope.currentTemp = "";
-			$scope.currentSum = "";
-			$scope.d1maxTemp = "";
-			$scope.d2maxTemp = "";
-			$scope.d3maxTemp = "";
-			$scope.d4maxTemp = "";
-			$scope.d5maxTemp = "";
-			$scope.d6maxTemp = "";
-			$scope.d7maxTemp = "";
-			$scope.d1minTemp = "";
-			$scope.d2minTemp = "";
-			$scope.d3minTemp = "";
-			$scope.d4minTemp = "";
-			$scope.d5minTemp = "";
-			$scope.d6minTemp = "";
-			$scope.d7minTemp = "";
-			$scope.d1summary = "";
-			$scope.d2summary = "";
-			$scope.d3summary = "";
-			$scope.d4summary = "";
-			$scope.d5summary = "";
-			$scope.d6summary = "";
-			$scope.d7summary = "";
+			$scope.cCity = "--";
+			$scope.currentTemp = "--";  
+			$scope.d1maxTemp = "--"; 
+			$scope.d2maxTemp = "--";
+			$scope.d3maxTemp = "--";
+			$scope.d4maxTemp = "--";
+			$scope.d5maxTemp = "--";
+			$scope.d6maxTemp = "--";
+			$scope.d1minTemp = "--";
+			$scope.d2minTemp = "--";
+			$scope.d3minTemp = "--";
+			$scope.d4minTemp = "--";
+			$scope.d5minTemp = "--";
+			$scope.d6minTemp = "--";
 		}	
 	})
 	
@@ -88,7 +78,7 @@ app.controller("forecastControl",function($scope, $http){
 	}
 
 	function cityfetch(){
-		$http.get("https://maps.googleapis.com/maps/api/place/autocomplete/json?input="+$scope.cityID+"&types=geocode&key=AIzaSyA2FyNv1PAM55HTQWNruhOQ7ybP_GRMSso"
+		$http.get("http://chathamweatherapi.azurewebsites.net/api/cities/search?byName="+$scope.cityID
 ).then(function(response){
 			$scope.cCity = response.data.predictions[0].description;
 			$scope.placeid = response.data.predictions[0].place_id;
@@ -99,7 +89,7 @@ app.controller("forecastControl",function($scope, $http){
 	
 
 	function placeDetail(){
-		$http.get("https://maps.googleapis.com/maps/api/place/details/json?placeid="+$scope.placeid+"&key=AIzaSyA2FyNv1PAM55HTQWNruhOQ7ybP_GRMSso"
+		$http.get("http://chathamweatherapi.azurewebsites.net/api/cities/"+$scope.placeid
 ).then(function(response){
 			$scope.long = response.data.result.geometry.location.lng;
 			$scope.lat = response.data.result.geometry.location.lat;	
@@ -108,32 +98,23 @@ app.controller("forecastControl",function($scope, $http){
 	}
 
 	function forecastfetch(){
-		$http.get("https://api.forecast.io/forecast/61b25c733c8f1e3632d05ed4ff0f51d9/"+$scope.lat+","+$scope.long).then(function(response){
+		$http.get("http://chathamweatherapi.azurewebsites.net/api/forecast?latitude="+$scope.lat+"&longitude="+$scope.long+"&source=FORECAST_IO").then(function(response){
 			$scope.currentTemp = response.data.currently.temperature; 
-			$scope.currentSum = response.data.currently.summary;
-			$scope.currentIcon = response.data.currently.icon;
-			$scope.d1maxTemp = response.data.daily.data[0].temperatureMax
-			$scope.d2maxTemp = response.data.daily.data[1].temperatureMax
-			$scope.d3maxTemp = response.data.daily.data[2].temperatureMax
-			$scope.d4maxTemp = response.data.daily.data[3].temperatureMax
-			$scope.d5maxTemp = response.data.daily.data[4].temperatureMax
-			$scope.d6maxTemp = response.data.daily.data[5].temperatureMax
-			$scope.d7maxTemp = response.data.daily.data[6].temperatureMax 
-			$scope.d1minTemp = response.data.daily.data[0].temperatureMin
-			$scope.d2minTemp = response.data.daily.data[1].temperatureMin
-			$scope.d3minTemp = response.data.daily.data[2].temperatureMin
-			$scope.d4minTemp = response.data.daily.data[3].temperatureMin
-			$scope.d5minTemp = response.data.daily.data[4].temperatureMin
-			$scope.d6minTemp = response.data.daily.data[5].temperatureMin
-			$scope.d7minTemp = response.data.daily.data[6].temperatureMin
-			$scope.d1summary = response.data.daily.data[0].summary;
-			$scope.d2summary = response.data.daily.data[1].summary;
-			$scope.d3summary = response.data.daily.data[2].summary;
-			$scope.d4summary = response.data.daily.data[3].summary;
-			$scope.d5summary = response.data.daily.data[4].summary;
-			$scope.d6summary = response.data.daily.data[5].summary;
-			$scope.d7summary = response.data.daily.data[6].summary;
 
+			$scope.d1minTemp = response.data.futureForecasts[0].temperatureMin;
+			$scope.d2minTemp = response.data.futureForecasts[1].temperatureMin;
+			$scope.d3minTemp = response.data.futureForecasts[2].temperatureMin;
+			$scope.d4minTemp = response.data.futureForecasts[3].temperatureMin;
+			$scope.d5minTemp = response.data.futureForecasts[4].temperatureMin;
+			$scope.d6minTemp = response.data.futureForecasts[5].temperatureMin;
+
+			$scope.d1maxTemp = response.data.futureForecasts[0].temperatureMax;
+			$scope.d2maxTemp = response.data.futureForecasts[1].temperatureMax;
+			$scope.d3maxTemp = response.data.futureForecasts[2].temperatureMax;
+			$scope.d4maxTemp = response.data.futureForecasts[3].temperatureMax;
+			$scope.d5maxTemp = response.data.futureForecasts[4].temperatureMax;
+			$scope.d6maxTemp = response.data.futureForecasts[5].temperatureMax;
+				
 		})
 	}
 });
